f46fd39190e8db1e6dce5ad86c6856f37a8eca82
= <radlohead@naver.com>
2019-06-05 18:33:50 +0900

    v-model 적용

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    <input type="checkbox" :value="value" @click="toggle">
+    <label>checkbox</label>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    toggle() {
+      this.$emit("input", !this.value);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
