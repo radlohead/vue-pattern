f711a7da640b0b2aa6747f19e72e94b03103ce93
= <radlohead@naver.com>
2019-06-06 15:01:42 +0900

    mixin 외부 컴포넌트로 분리

--- src/components/mixin/App.vue
+++ src/components/mixin/App.vue
@@ -5,48 +5,11 @@
 </template>
 
 <script>
-const AppChild = {
-  template: `<div>
-    <h2>Here I am!</h2>
-    <slot />
-  </div>`
-};
-
-const AppModal = `<div id="modal">
-      <h3>Let's trigger this here modal!</h3>
-      <button @click="toggleShow">
-        <span v-if="isShowing">Hide child</span>
-        <span v-else>Show child</span>
-      </button>
-      <app-child v-if="isShowing" class="modal">
-        <button @click="toggleShow">Close</button>
-      </app-child>
-    </div>`;
-
-const toggle = {
-  data() {
-    return {
-      isShowing: false
-    };
-  },
-  methods: {
-    toggleShow() {
-      this.isShowing = !this.isShowing;
-    }
-  }
-};
-
-const Modal = {
-  template: AppModal,
-  mixins: [toggle],
-  components: {
-    appChild: AppChild
-  }
-};
+import AppModal from "./AppModal";
 
 export default {
   components: {
-    appModal: Modal
+    AppModal
   }
 };
 </script>
---
+++ src/components/mixin/AppModal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="modal">
+    <h3>Let's trigger this here modal!</h3>
+    <button @click="toggleShow">
+      <span v-if="isShowing">Hide child</span>
+      <span v-else>Show child</span>
+    </button>
+    <app-child v-if="isShowing" class="modal">
+      <button @click="toggleShow">Close</button>
+    </app-child>
+  </div>
+</template>
+
+<script>
+const AppChild = {
+  template: `<div>
+    <h2>Here I am!</h2>
+    <slot />
+  </div>`
+};
+
+const toggle = {
+  data() {
+    return {
+      isShowing: false
+    };
+  },
+  methods: {
+    toggleShow() {
+      this.isShowing = !this.isShowing;
+    }
+  }
+};
+
+export default {
+  mixins: [toggle],
+  components: {
+    AppChild
+  }
+};
+</script>
+
