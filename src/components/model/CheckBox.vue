d112dbb798d87756c82b5805d0834592695a98ff
= <radlohead@naver.com>
2019-06-26 17:01:24 +0900

    props type test

--- src/components/model/CheckBox.vue
+++ src/components/model/CheckBox.vue
@@ -1,13 +1,19 @@
 <template>
   <article>
     <input type="checkbox" :value="value" @click="toggle">
+    <span>props value: {{ value }}</span>
     <label>checkbox</label>
   </article>
 </template>
 
 <script>
 export default {
-  props: ["value"],
+  props: {
+    value: {
+      type: Boolean | String,
+      default: "props value값이 없으면 이 텍스트가 보여집니다."
+    }
+  },
   methods: {
     toggle() {
       this.$emit("input", !this.value);
